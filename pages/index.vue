33463000e09aa29ee5e493911d66f4c168dac8eb
Martins Zeltins <queeeeenz@gmail.com>
2024-12-03 22:09:55 +0200

    Testing

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@
     parsing (!) - (backend errors
     dont get added)
 [ ] Deeply nested objects, arrays
+[ ] Check Vuelidate issues for ideas
 [x] Conditional rules
 [x] Dynamic, custom error messages
     with i18n, (minWeight example)
